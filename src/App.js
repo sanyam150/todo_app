94966b4bb32811e9cc8b914c752c1b3f098b1643
sanyam150 <sanyamvirmani1992@gmail.com>
2024-05-30 16:31:41 +0530

    Added Loader

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchTodos } from './redux/reducers/todoReducers';
 import TableComponent from './Components/Table';
 function App() {
   const dispatch = useDispatch();
+  const { loading } = useSelector((state) => state.todos);
+
   useEffect(() => {
     dispatch(fetchTodos());
   }, [dispatch]);
   return (
     <div>
-      <TableComponent />
+      {loading ? (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            height: '100vh',
+            alignItems: 'center',
+          }}
+        >
+          <div class='spinner-border' role='status'></div>
+        </div>
+      ) : (
+        <div style={{ marginTop: '10px' }}>
+          <TableComponent />
+        </div>
+      )}
     </div>
   );
 }
